db263b6c31038d34acffc14d6831c072699b0130
AdrianUACJ <adrian.sanabria@uacj.mx>
2026-01-20 13:14:08 -0700

    Asuntos internos

--- Application/Class1.cs
+++ Application/Class1.cs
@@ -4,6 +4,6 @@ public class Class1
 {
     public Class1()
     {
-        Infrastructure.Class1 c = new Infrastructure.Class1();
+        //Adrián Guzmán Sanabria -- "Yo no entendí bien la película ¿Los policías sabían que asuntos internos les tendía una trampa?"
     }
 }

892f4e4a34ff0fa79b8c25a7ed03c71f98e8c645
Gustavo Alonso Gamero <gustavo.gamero@uacj.mx>
2026-01-20 13:24:07 -0700

    Un commit

--- Application/Class1.cs
+++ Application/Class1.cs
@@ -4,6 +4,6 @@ public class Class1
 {
     public Class1()
     {
-        Infrastructure.Class1 c = new Infrastructure.Class1();
+        //Gustavo Gamero. Almacenamiento temporal de credenciales, por seguridad, taparse los ojos. --aScV--?dsesSSASdsDF-xsfasd.dsfwsdfasdf--wsasdf
     }
 }

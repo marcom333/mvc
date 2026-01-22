7a16708ab71cead768baadf9f731516de286c6ab
Pako <francisco.mora@uacj.mx>
2026-01-21 19:58:57 -0700

    Wendnesday Homework

--- Application/Entities/Product.cs
+++ Application/Entities/Product.cs
@@ -12,16 +12,16 @@ namespace Application.Entities
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "El nombre es Obligatorio.")]
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
         public string? Name { get; set; }
 
-        [Required(ErrorMessage = "La descripción es Obligatoria.")]
+        [Required(ErrorMessage = "La descripción es obligatoria.")]
         public string? Description { get; set; }
 
-        [Required(ErrorMessage = "El precio es Obligatorio.")]
+        [Required(ErrorMessage = "El precio es obligatorio.")]
         public int? Price { get; set; }
 
-        [Required(ErrorMessage = "La categoría es Obligatoria.")]
+        [Required(ErrorMessage = "La categoría es obligatoria.")]
         public int? CategoryId { get; set; }
 
         public int? UserId { get; set; }

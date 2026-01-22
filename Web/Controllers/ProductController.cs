910614a8ea25613fac4fd27584b7e00936aab713
PakoMoraUACJ <francisco.mora@uacj.mx>
2026-01-21 19:41:09 -0700

    .

---
+++ Web/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> products = [];
+
+            for(int i=0; i<10; i++)
+            {
+                products.Add(new() {
+                    Name = "Product #" + i
+                });
+            }
+
+            return View(products);
+        }
+    }
+}

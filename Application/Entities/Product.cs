2bdd8e220b6af360ae53630945bb0ecf2c288b87
Pako <francisco.mora@uacj.mx>
2026-01-21 19:57:21 -0700

    Wendnesday Homework

--- Application/Entities/Product.cs
+++ Application/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,22 @@ namespace Application.Entities
 {
     public class Product
     {
-        public string Name { get; set; } = "";
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es Obligatorio.")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "La descripción es Obligatoria.")]
         public string? Description { get; set; }
-        public int Precio { get; set; }
-        public  int CategoryId { get; set; }
-        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "El precio es Obligatorio.")]
+        public int? Price { get; set; }
+
+        [Required(ErrorMessage = "La categoría es Obligatoria.")]
+        public int? CategoryId { get; set; }
+
+        public int? UserId { get; set; }
 
     }
 }
--- Web/Controllers/ProductController.cs
+++ Web/Controllers/ProductController.cs
@@ -1,23 +1,78 @@
 ﻿using Application.Entities;
 using Microsoft.AspNetCore.Mvc;
-using System.Net.Http.Headers;
 
-namespace Web.Controllers
+namespace Web.Controllers;
+
+public class ProductController : Controller
 {
-    public class ProductController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
+        List<Product> productos = new List<Product>();
+        for (int i = 0; i < 10; i++)
         {
-            List<Product> products = [];
-
-            for(int i=0; i<10; i++)
+            productos.Add(new()
             {
-                products.Add(new() {
-                    Name = "Product #" + i
-                });
-            }
-
-            return View(products);
+                Name = $"Product #{i + 1}"
+            });
         }
+        return View(productos);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Store(Product product)
+    {
+        if (!ModelState.IsValid)
+        {
+            TempData["error"] = "El producto no fue almacenado!";
+            return View("Create", product);
+        }
+
+        TempData["success"] = "El producto fue almacenado Exitosamente!";
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet("Product/Edit/{id:int}")]
+    public IActionResult Edit(int? id)
+    {
+        Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff} | {Request.Method} | {Request.Path} | id={id}");
+
+        if (id == null)
+        {
+            TempData["error"] = "El producto no fue encontrado!";
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Busqueda de producto
+        Product producto = new Product()
+        {
+            Name = "Lata",
+            Description = "Lata de .....",
+            Price = 10,
+            CategoryId = 5,
+            UserId = 10
+        };
+
+        return View(producto);
+    }
+
+    [HttpPost]
+    public IActionResult Update(Product product)
+    {
+        if (!ModelState.IsValid)
+        {
+            TempData["error"] = "El producto no fue actualizado!";
+            return View("Edit", product);
+        }
+
+        Console.WriteLine($"product id: {product.Id}");
+
+        TempData["success"] = "El producto fue actualizado Exitosamente!";
+        return RedirectToAction(nameof(Index));
     }
 }
